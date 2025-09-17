1b903c45a5043420af15a5c5561614e720666f89
Mraanderson <ajaxupgrades@gmail.com>
2025-09-17 20:45:23 +0100

    Update routes_gallery.py

--- app/routes_gallery.py
+++ app/routes_gallery.py
@@ -3,12 +3,12 @@ from .utils import get_all_images, IMAGES_DIR
 
 bp = Blueprint('gallery', __name__)
 
-@bp.route("/")
-@bp.route("/gallery")
+@bp.route("/", endpoint='gallery')
+@bp.route("/gallery", endpoint='gallery')
 def gallery():
     return render_template("gallery.html", image_names=get_all_images())
 
-@bp.route("/images/<path:filename>")
+@bp.route("/images/<path:filename>", endpoint='serve_image')
 def serve_image(filename):
     return send_from_directory(IMAGES_DIR, filename)
     
