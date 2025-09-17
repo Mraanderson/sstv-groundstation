6c399bc3d120cf19bee998c828c671b348198b32
Mraanderson <ajaxupgrades@gmail.com>
2025-09-17 20:17:19 +0100

    Create routes_gallery.py

---
+++ app/routes_gallery.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template, send_from_directory
+from .utils import get_all_images, IMAGES_DIR
+
+bp = Blueprint('gallery', __name__)
+
+@bp.route("/")
+@bp.route("/gallery")
+def gallery():
+    return render_template("gallery.html", image_names=get_all_images())
+
+@bp.route("/images/<path:filename>")
+def serve_image(filename):
+    return send_from_directory(IMAGES_DIR, filename)
+  
