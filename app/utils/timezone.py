8ea22dd7f448fe6c97866a29ba68bd35c4fdc2e7
Mraanderson <ajaxupgrades@gmail.com>
2025-09-18 10:45:03 +0100

    Update timezone.py

--- app/utils/timezone.py
+++ app/utils/timezone.py
@@ -1,1 +1,13 @@
+from timezonefinder import TimezoneFinder
 
+def get_timezone_for_coords(lat, lon):
+    """
+    Given latitude and longitude, return the timezone name.
+    Returns None if it cannot be determined.
+    """
+    try:
+        tf = TimezoneFinder()
+        return tf.timezone_at(lat=lat, lng=lon)
+    except Exception:
+        return None
+      
