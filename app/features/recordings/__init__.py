1dad0c044fb006c6aa339dcc48d4786d0a1a3471
Mraanderson <ajaxupgrades@gmail.com>
2025-09-23 20:24:53 +0100

    Update __init__.py

--- app/features/recordings/__init__.py
+++ app/features/recordings/__init__.py
@@ -1,5 +1,8 @@
 from flask import Blueprint
 
-bp = Blueprint("recordings", __name__, template_folder="../../templates/recordings")
+# Create the blueprint for the recordings feature
+# Templates will be loaded from app/features/recordings/templates/
+bp = Blueprint("recordings", __name__, template_folder="templates")
 
+# Import routes so they are registered with the blueprint
 from app.features.recordings import routes
