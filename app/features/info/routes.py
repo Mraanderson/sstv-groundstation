27c69ebe0a2e676f92895e554d9ad5090450aa44
Mraanderson <ajaxupgrades@gmail.com>
2025-10-02 22:38:14 +0100

    Fix info

--- app/features/info/routes.py
+++ app/features/info/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 
-bp = Blueprint('info', __name__, template_folder='templates/info')
+# Use the package `templates` folder so the template path `info/info.html` resolves
+bp = Blueprint('info', __name__, template_folder='templates')
 
 @bp.route('/info')
 def info():
